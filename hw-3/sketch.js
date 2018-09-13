35b4bfef5529644ef3cb647a4366719db3493acf
Ambydo <elijah.garcia.ghs@gmail.com>
2018-09-12 21:35:19 -0600

    Part 1 Homework hello world

---
+++ hw-3/sketch.js
@@ -0,0 +1,15 @@
+//create a function named setup
+function setup() {
+//Setting the resolution to that of my first PC monitor, a classic CRT
+  createCanvas ( 800, 600 );
+// Creating a non-popping beige background
+  background( 'beige' );
+}
+//filling the canvas with text content
+function draw() {
+//Say hello to the world
+  text( "Hello, World!", 10, 15 );
+//Ask the user how they're doing
+  text( "How are you?", 20, 30 );
+
+}
